1496ca0b57bd036568701c40e0e30fab58d1de78
zangqilong <zql@liulishuo.com>
2015-04-29 16:52:51 +0800

    Initial Commit

---
+++ HotGirls/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HotGirls
+//
+//  Created by 臧其龙 on 15/4/29.
+//  Copyright (c) 2015年 臧其龙. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
